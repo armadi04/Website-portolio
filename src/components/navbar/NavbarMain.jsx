0642154f82041cba9e44f7ed4589cf60f68730fa
Armadi Nurhidayat <armadinurhidayat04@gmail.com>
2025-04-17 11:30:36 +0700

    navbar mobile

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux";
 const NavbarMain = () => {
   const menuOpen = useSelector((state) => state.menu.menuOpen);
   return (
-    <nav className="max-w-[1300px] mx-auto w-full  px-4 fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2">
+    <nav className="max-w-[1300px] mx-auto w-full  px-4 fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2 sm:my-1">
       <div className="flex justify-between w-full max-w-[1400px] mx-0  bg-black items-center p-1 rounded-r-full rounded-l-full border-orange border-[0.5px]">
         <NavbarLogo />
         <div className={`${menuOpen ? "sm:block" : "sm:hidden"} lg:block`}>
